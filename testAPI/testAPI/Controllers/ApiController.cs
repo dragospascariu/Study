36d464913a778a57cd45dd882f31f84e32869629
Dragos <pasdragos@yahoo.com>
2022-02-01 23:58:39 +0100

    wizkidsTaskBackend

---
+++ testAPI/testAPI/Controllers/ApiController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WordPrediction.DbConnection;
+using WordPrediction.Service;
+
+
+
+namespace WordPrediction.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+
+    public class ApiController : ControllerBase
+    {
+        [HttpGet, Route("Backend")]
+        public IActionResult DbGet(string text, string locale)
+        {
+            var dbConnection = new DatabaseConnection();
+            var result = dbConnection.GetPredictionsForWord(text);
+            var webService = new WebPredictionService();
+            var webServiceResult = webService.GetWizkidsWordPrediction(text, locale).Result;
+            var mergedResult = new[] { (object)result, (object)webServiceResult};
+            return Ok(mergedResult);
+        }
+    }
+}
